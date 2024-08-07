bbf4a825b4ac2d38403341e11438c9d397993afa
weisiqian <1535658549@qq.com>
2024-08-07 12:54:29 +0800

    rustup

--- gvm.sh
+++ gvm.sh
@@ -1,3 +1,13 @@
-sudo apt install binutils gcc make -y
-sudo apt-get install bison -y
+sudo apt-get install curl git mercurial make binutils bison gcc build-essential
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+
+# 安装 Go 1.20+
+# Go 1.20+ 需要 go1.17.3+。使用以下命令：
+gvm install go1.4 -B
+gvm use go1.4
+export GOROOT_BOOTSTRAP=$GOROOT
+gvm install go1.17.13
+gvm use go1.17.13
+export GOROOT_BOOTSTRAP=$GOROOT
+gvm install go1.20
+gvm use go1.20
---
+++ rustup.sh
@@ -0,0 +1,4 @@
+wget https://static.rust-lang.org/rustup/rustup-init.sh
+chmod +x rustup-init.sh
+./rustup-init.sh
+$HOME/.cargo/env
